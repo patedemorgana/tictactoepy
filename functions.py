854eb7c97979588cf3f7919c09e3e052d92504b0
patedemorgana <gabriel.volpate@gmail.com>
2022-09-07 12:17:13 -0300

    adfsdf:wq

--- functions.py
+++ functions.py
@@ -2,6 +2,10 @@ import pygame
 import v 
 import time
 
+for linha in range(10):
+    v.malha.append([])
+    for v.coluna in range(10):
+        v.malha[v.linha].append(0)
 
 pygame.init()
 pygame.display.set_caption("joguin da veia")
@@ -9,24 +13,20 @@ pygame.display.set_caption("joguin da veia")
 def loop():
     v.tela.fill(v.w)
     while not v.jogo:
-        
-        pygame.display.update()
 
-        
+        pygame.display.update()    
         for i in pygame.event.get():
             if i.type == pygame.KEYDOWN:
                 if i.key == pygame.K_q:
                     v.jogo = True
-
-
-        for i in pygame.event.get():
-
-            if i.type == pygame.QUIT:
-                v.jogo = True
+            elif i.type == pygame.MOUSEBUTTONDOWN:  
+                v.malha[v.linha][v.coluna] = 1
+                print("Clicado,", v.pos, "Coordenadas:", v.linha, v.coluna)
+        v.tela.fill(w)
 
 
 
-        print(v.jogo)
+        
         
         pygame.display.update()
 
--- v.py
+++ v.py
@@ -4,7 +4,21 @@ jogo = False
 
 width = 500
 height = 400
+margem = 5
 
+xo = 'x'
+
+vencedor = None
+empate = None
+
+b = (0, 0 , 0)
 w = (255, 255, 255)
 
+tabuleiro = [[None]*3, [None]*3, [None]*3]
+malha = []
+
 tela = pygame.display.set_mode((width, height))
+
+pos = pygame.mouse.get_pos()
+coluna = pos [0] // (width + margem)
+linha = pos [1] // (height + margem)
